dcb9b0eb7f262cca71d8b54f2b2d7545be416173
CoderDream <coderdream@gmail.com>
2014-11-08 15:44:12 +0800

    V1.2 程序主界面的UI设计

--- README.md
+++ README.md
@@ -5,6 +5,46 @@ MobileSafe
 
 ----------
 
+## 版本1.2 ##
+
+更新日期
+
+- 20141108
+
+功能
+
+- 程序主界面的UI设计
+
+新增文件
+
+No  |文件名  | 说明
+--- |------------- | -------------
+1  |bg_normal_pressed.xml| 样式文件
+2  |bg_normal.xml  | 样式文件
+3  |bg_selector.xml | 样式文件
+4  |widget01.png | 图片文件
+5  |widget02.png | 图片文件
+6  |widget03.png | 图片文件
+7  |widget04.png | 图片文件
+8  |widget05.png | 图片文件
+9  |widget06.png | 图片文件
+10  |widget07.png | 图片文件
+11  |widget08.png | 图片文件
+12  |widget09.png | 图片文件
+13  |devide_line.png | 图片文件
+14  |main_item.xml | 布局文件
+15  |MainAdapter.java | 图片适配器
+
+
+修改文件
+
+No  |文件名  | 说明
+--- |------------- | -------------
+1  |main.xml | 布局文件
+1  |MainActivity.java  | 响应焦点停留事件
+
+----------
+
 ## 版本1.1.6 ##
 
 更新日期
--- src/com/coderdream/mobilesafe/activity/MainActivity.java
+++ src/com/coderdream/mobilesafe/activity/MainActivity.java
@@ -3,23 +3,32 @@ package com.coderdream.mobilesafe.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.GridView;
 
 import com.coderdream.mobilesafe.R;
+import com.coderdream.mobilesafe.adapter.MainAdapter;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	/**
+	 * 显示主界面中的九大模块的GridView
+	 */
+	private GridView gv_main;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		gv_main = (GridView) findViewById(R.id.gv_main);
+		// 为gv_main对象设置一个适配器，该适配器的作用是用于为每个item填充对应的数据
+		gv_main.setAdapter(new MainAdapter(this));
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
